1be8016c7fca8c3e58b928887140f152686d0080
Nicholas Reich <nick@schoolph.umass.edu>
2020-02-11 13:37:14 +0100

    updates to example code.

--- inst/scripts/example-code.R
+++ inst/scripts/example-code.R
@@ -2,13 +2,33 @@ library(desurvdata)
 library(ggplot2)
 
 diseases <- get_diseases()
-weekly_noro <- get_weekly_timeseries(disease = "Noroviral gastroenteritis")
-weekly_rsv <- get_weekly_timeseries(disease = "RSV-Infection")
+weekly_noro <- get_weekly_timeseries(disease = "Noroviral gastroenteritis", years=2010:2020)
+weekly_lyme <- get_weekly_timeseries(disease = "Lyme disease", years=2010:2020)
+weekly_camp <- get_weekly_timeseries(disease = "Campylobacteriosis", years=2014:2020)
+weekly_salmon <- get_weekly_timeseries(disease = "Salmonellosis", years=2014:2020)
+weekly_flu <- get_weekly_timeseries(disease = "Influenza", years=2010:2020)
 
 ggplot(weekly_noro, aes(x=date, y=value)) + 
     geom_line() +
-    facet_wrap(variable~.)
+    facet_wrap(variable~.) +
+    xlab(NULL)
 
-ggplot(weekly_rsv, aes(x=date, y=value)) + 
+ggplot(weekly_flu, aes(x=date, y=value)) + 
     geom_line() +
-    facet_wrap(variable~.)
+    facet_wrap(variable~.) +
+    xlab(NULL)
+
+ggplot(weekly_lyme, aes(x=date, y=value)) + 
+    geom_line() +
+    facet_wrap(variable~.) +
+    xlab(NULL)
+
+ggplot(weekly_camp, aes(x=date, y=value)) + 
+    geom_line() +
+    facet_wrap(variable~.) +
+    xlab(NULL)
+
+ggplot(weekly_salmon, aes(x=date, y=value)) + 
+    geom_line() +
+    facet_wrap(variable~.) +
+    xlab(NULL)
